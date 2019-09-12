b51299ae09979fe75c0577ffbcc404f1a26739af
saitoreina <reina.saito@di-v.co.jp>
2019-09-12 20:58:53 +0900

    enzanshi.edit
    if文を三項演算子へ変更

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,43 +1,23 @@
 $(function(){
   function buildHTML(message){
-    if ( message.image ) {
-      var html =
-        `<div class="message" data-message-id=${message.id}>
-          <div class="upper-message">
-            <div class="upper-message__user-name">
-              ${message.user_name}
-            </div>
-            <div class="upper-message__date">
-              ${message.date}
-            </div>
-          </div>
-          <div class="lower-message">
-            <p class="lower-message__content">
-              ${message.content}
-            </p>
-          </div>
-          <img src=${message.image} >
-        </div>`
+
+    var content = message.content ? `<p class="lower-message__content">${message.content}</p>` : "";
+    var image = message.image.url ? `<img class="lower-message__image" src= ${message.image.url}>` : "";
+    var html = `<div class="message" data-message-id="${message.id}">
+                  <div class="upper-message">
+                    <div class="upper-message__user-name">
+                      ${ message.user_name}
+                    </div>
+                    <div class="upper-message__date">
+                      ${message.created_at}
+                    </div>
+                  </div>
+                  <div class="lower-message">
+                    ${content}
+                    ${image}
+                  </div>
+                </div>`
       return html;
-    } else {
-      var html =
-        `<div class="message" data-message-id=${message.id}>
-          <div class="upper-message">
-            <div class="upper-message__user-name">
-              ${message.user_name}
-            </div>
-            <div class="upper-message__date">
-              ${message.date}
-            </div>
-          </div>
-          <div class="lower-message">
-            <p class="lower-message__content">
-              ${message.content}
-            </p>
-          </div>
-        </div>`
-      return html;
-    };
   }
 $('.new_message').on('submit', function(e){
     e.preventDefault();
