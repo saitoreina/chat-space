4e22f00b2f8f2c1a022d72d6d7981bfb9361e1d7
saitoreina <reina.saito@di-v.co.jp>
2019-09-25 17:08:10 +0900

    delete.console
    console.log消し忘れ

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -33,7 +33,6 @@ $(function(){
       contentType: false
     })
     .done(function(data){
-      console.log(data)
       var html = buildHTML(data);
       $('.messages').append(html);
       $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');
